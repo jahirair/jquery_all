a4896c5a5fd4d20931628001bd2dbee534d9459f
jahirair <jahirair@gmail.com>
2020-09-22 10:12:10 +0600

    Create all.js

---
+++ all.js
@@ -0,0 +1,29 @@
+// magnifiq popup 
+<script src="assets/js/magnific-popup.min.js"></script>
+
+//magnific popup activation
+
+  $('.project-popup').magnificPopup({
+	  type:'image',
+	  gallery:{
+		enabled:true
+	  },
+	  
+	  });
+    
+	  $('.video-popup').magnificPopup({
+	  type:'iframe'
+	  
+	  });
+    
+	// html for magnific popup
+  
+   <a href="https://www.youtube.com/watch?v=CRBsaGSOGwQ" class="video-popup">
+      <i class="far fa-play-circle"></i>
+   </a>
+   
+   <a href="assets/img/banner1.750.jpg" class="project-popup">
+        <i class="far fa-plus-square"></i>
+   </a>
+                     
+                     
